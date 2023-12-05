786aa03bcedf0c9db0ff7644eeff2cbf17b91d42
RafeedN <rafeed.n1@gmail.com>
2023-12-05 17:48:42 -0500

    Completed header files

--- AED.h
+++ AED.h
@@ -1,4 +1,17 @@
 #ifndef AED_H
 #define AED_H
 
+class AED{
+public:
+    AED();
+    void powerOn();
+    void initiateSelfTest();
+    void analyzeHeartRhythm();
+    void deliverShock();
+private:
+    bool isOn;
+    bool eletrodesPlaced;
+    bool shockAdvised;
+};
+
 #endif // AED_H
--- Patient.h
+++ Patient.h
@@ -1,4 +1,20 @@
 #ifndef PATIENT_H
 #define PATIENT_H
 
+#include <string>
+using namespace std;
+
+class Patient{
+public:
+    Patient();
+    void isResponsive();
+
+private:
+    string name;
+    bool isHairy;
+    bool responsive;
+    bool shockResult;
+
+};
+
 #endif // PATIENT_H
